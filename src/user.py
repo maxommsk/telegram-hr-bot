5e0d9a446afadb13e6845cd04efd232e1b5bc795
maxommsk <119955040+maxommsk@users.noreply.github.com>
2025-07-22 09:13:39 +0300

    Update user.py

--- src/user.py
+++ src/user.py
@@ -1,8 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-
-db = SQLAlchemy()
+from core import db
 
 class User(db.Model):
     __tablename__ = 'users'
